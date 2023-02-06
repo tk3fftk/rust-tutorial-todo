5f89b23be3a7051eaf26492af71cc2ec05e9527a
tk3fftk <tktk.stereoman@gmail.com>
2023-02-07 08:58:02 +0900

    use sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, ResponseError};
 use askama::Template;
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::params;
 use thiserror::Error;
 
 struct TodoEntry {
@@ -17,13 +20,35 @@ struct IndexTemplate {
 enum MyError {
     #[error("Failed to render HTML")]
     AskamaError(#[from] askama::Error),
+
+    #[error("Failed get connection")]
+    ConnectionPoolError(#[from] r2d2::Error),
+
+    #[error("Failed SQL execution")]
+    SQLiteError(#[from] rusqlite::Error),
 }
 
 impl ResponseError for MyError {}
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, MyError> {
+async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+
+    // SQL 文を Prepared Statement (?が入ってるようなやつのイメージ) に変換
+    let mut statement = conn.prepare("SELECT id, text FROM todo")?;
+
+    let rows = statement.query_map(params![], |row| {
+        let id = row.get(0)?;
+        let text = row.get(1)?;
+        Ok(TodoEntry { id, text })
+    })?;
+
     let mut entries = Vec::new();
+    for row in rows {
+        entries.push(row?);
+    }
+
+    /*
     entries.push(TodoEntry {
         id: 1,
         text: "first entry".to_string(),
@@ -32,6 +57,7 @@ async fn index() -> Result<HttpResponse, MyError> {
         id: 2,
         text: "second entry".to_string(),
     });
+    */
 
     let html = IndexTemplate { entries };
     let response_body = html.render()?;
@@ -42,9 +68,28 @@ async fn index() -> Result<HttpResponse, MyError> {
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+async fn main() -> Result<(), actix_web::Error> {
+    let manager = SqliteConnectionManager::file("todo.db");
+    let pool = Pool::new(manager).expect("Failed to initialize the connection pool.");
+    let conn = pool
+        .get()
+        .expect("Failed to get the connection from the pool");
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS todo (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL
+        )",
+        params![],
+    )
+    .expect("Failed to create a table todo");
+
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+            .app_data(actix_web::web::Data::new(pool.clone())) // https://stackoverflow.com/questions/73255421/actix-web-requested-application-data-is-not-configured-correctly-view-enable-d
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await?;
+    Ok(())
 }
